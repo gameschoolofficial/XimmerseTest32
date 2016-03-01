e006dfcbe4315fcc71370b1eea030d280e965f7c
charlietrinh <charlie.t.trinh@gmail.com>
2016-02-29 20:33:27 -0800

    Working Test Scene with directions correct. Made 'CompleteHandsCamera' prefab

---
+++ Assets/_scripts/InputTest.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using Ximmerse.CrossInput;
+
+public class InputTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//printTriggerInput ();  
+	
+	}
+
+	void printTriggerInput()
+	{
+		print("Left Trigger is " + CrossInputManager.GetButton ("Left_Trigger"));
+		print("Right Trigger is " + CrossInputManager.GetButton ("Right_Trigger"));
+	}
+}
