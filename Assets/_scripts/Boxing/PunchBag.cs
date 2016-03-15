b14739f324d93d9c9e73ab2150a5d0b378e9a3c1
Jason <software@positrongames.com>
2016-03-15 16:23:28 -0700

    Creating Boxing manager - Setup basic game then set up the rest.

---
+++ Assets/_scripts/Boxing/PunchBag.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PunchBag : MonoBehaviour {
+	public bool selected;
+	public int pointsGained;
+	public int hits;
+
+	public Material Red;
+	public Material Neutral;
+
+	// Use this for initialization
+	void Start () {
+		enterSelected ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	
+	}
+
+	void OnCollisionExit(Collision col)
+	{
+		print ("collision");
+		if (selected) {
+			pointsGained += 100;
+			hits++;
+		}
+	}
+
+	void enterSelected()
+	{
+		selected = true; 
+		//color = red;
+		GetComponent<Renderer>().material = Red;
+	}
+
+	void exitSelected()
+	{
+		selected = false;
+		//color = none;
+		GetComponent<Renderer>().material = Neutral;
+	}
+}
